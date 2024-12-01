c1a3e1c52f8eb66db04220e5932c1a1574de4a32
will <william.steven51218@gmail.com>
2024-12-01 15:35:12 +0000

    Day 1 aoc code

---
+++ Day1.py
@@ -0,0 +1,26 @@
+
+
+list1 = []
+list2 = []
+with open("Inputs/Day1","r") as file:
+    for i in file.readlines():
+        list1.append(int(i[:6]))
+        list2.append(int(i[8:-1]))
+list1a = list1.copy()
+list2a = list2.copy()
+
+list1.sort()
+list2.sort()
+
+total = 0
+for i in range(len(list1)):
+    total += abs(list1[i]-list2[i])
+
+print(total)
+
+total = 0
+for i in list1a:
+    a = list2a.count(i)
+    total += i*a
+
+print(total)
